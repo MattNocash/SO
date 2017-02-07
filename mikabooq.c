7d5c137cc89c9b4048ece7ce79c47abc3731fa20
AA14142 <AA14142@users.noreply.github.com>
2017-02-07 13:41:16 +0100

    Da controllare.

--- mikabooq.c
+++ mikabooq.c
@@ -4,23 +4,39 @@
 #include "mikabooq.h"
 #include "const.h"
 
-
+//Allocazione statica delle MAXPROC strutture in memoria.
 struct pcb_t pcb_mem[MAXPROC];
+
 typedef struct pcb_t * ptr_pcb_t;
+
+//Puntatore alla lista libera.
 ptr_pcb_t ptr_pcblistfree_head;
+
+//Puntatore al processo "root".
+ptr_pcb_t root;
+
 //Definizione della funzione proc_init
 struct pcb_t *proc_init(void){
-	ptr_pcb_t tmpnext, tmpprev;
-	ptr_pcblistfree_head = pcb_mem;
-	tmp = ptr_pcblistfree_head;
-	pcb_mem[0]->p_siblings.next = &pcb_mem[1];
-	pcb_mem[1]->p_siblings.prev = &pcb_mem[0];
+	short int i;
 
+	//Collego il puntatore al primo elemento della lista libera.
+	ptr_pcblistfree_head = &pcb_mem[1];
 
+	//Collego i puntatori p_siblings formando la lista libera.
+	for(i = 1; i < MAXPROC-2; i++){
+		pcb_mem[i]->p_siblings.next = &pcb_mem[i+1];
+		pcb_mem[i+1]->p_siblings.prev = &pcb_mem[i];
+	}
+
+	// Restituisco il puntatore al processo root.
+	return &pcb_mem[0];
 }
 
+
+
+-------------- MAIN ---------------
 int main{
-struct pcb_t * root = proc_init();
+ptr_pcb_t root = proc_init();
 
 
 }
