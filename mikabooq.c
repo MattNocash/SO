de2d78ca081eacf52e0ac7e63f8d407bd77f9398
AA14142 <AA14142@users.noreply.github.com>
2017-02-18 17:10:31 +0100

    Quasi finito i messaggi

--- mikabooq.c
+++ mikabooq.c
@@ -1,99 +1,231 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "listx.h"
 #include "mikabooq.h"
 #include "const.h"
 
-//Allocazione statica delle MAXPROC, MAXTHREAD e MAXMSG strutture in memoria.
-struct pcb_t pcb_mem[MAXPROC];
-struct tcb_t tcb_mem[MAXTHREAD];
-struct msg_t msg_mem[MAXMSG];
+//Free list heads, 3 sounds like free
+struct list_head pcb_3;
+struct list_head tcb_3;
+struct list_head msg_3;
 
-// Head lista libera.
-struct list_head listfree_head;
+/************************************** PROC MGMT ************************/
 
-//Puntatore al processo "root".
-struct pcb_t * root;
+/* initialize the data structure */
+/* the return value is the address of the root process */
+struct pcb_t *proc_init(void)
+{
+	static struct pcb_t pcb_array[MAXPROC]; //Static array of pcbs containing MAXPROC elements
+	
+	INIT_LIST_HEAD(&pcb_3); //Initialize pcb_3 head
+	
+	struct pcb_t *root = &pcb_array[0]; //Assign first element of free list to root
+	root->p_parent = NULL; //Root process has no parent
+	INIT_LIST_HEAD(&root->p_threads); //Initialize root threads
+	INIT_LIST_HEAD(&root->p_children); //Initialize root children
 
-//Definizione della funzione proc_init
-struct pcb_t *proc_init(void){
-	short int i;
-	//Inizializzo la testa della coda dei processo liberi, poi utilizzata come lista.
-	INIT_LIST_HEAD(listfree_head);
-	//Collego i puntatori p_siblings formando la lista libera.
-	for(i = 0, i < MAXPROC, i++){
-		//Setto a NULL i puntatori p_parent.
-		INIT_LIST_HEAD(&(pcb_mem[i].p_siblings));
-		INIT_LIST_HEAD(&(pcb_mem[i].p_children));
-		INIT_LIST_HEAD(&(pcb_mem[i].p_threads));
-		pcb_mem[i].p_parent = NULL;
-		if(i > 0)
-			list_add_tail(&(pcb_mem[i].p_siblings), &listfree_head);
+	for(int i = 1; i < MAXPROC; i++) //Iterate for MAXPROC-1 times
+	{
+		struct pcb_t *new_pcb = &pcb_array[i]; // Pointer to working element of array
+		list_add(&new_pcb->p_siblings, &pcb_3); //Use p_siblings to manage pcb free list
 	}
-	// Restituisco il puntatore al processo root.
-	return &pcb_mem[0];
+	
+	return root; // Return pointer to root process
 }
 
-struct pcb_t *proc_alloc(struct pcb_t *p_parent){
-	struct pcb_t * new = NULL;
-	struct list_head * curr; 
-	if(p_parent != NULL){
-		// Prendo dalla lista il primo processo libero.
-		curr = list_next(&listfree_head);
-		//Se la lista dei processi liberi non è vuota passo eseguo le operazioni.
-		if(new != NULL){
-			//Cancello dalla lista libera il processo.
-			list_del(curr);
-			//new = container_of(curr, struct pcb_t, p_siblings);
-			// Lo aggiungo alla lista p_children del processo puntato da p_parent.
-			list_add(curr, &(p_parent->p_children));
-			//Ottengo il puntatore al nuovo processo attivo.
-			new = container_of(curr, struct pcb_t, p_siblings);
-			//Assegno a p_parent del nuovo processo il processo padre.
-			new.p_parent = p_parent;
-		}
-		// Ritorno il puntatore al nuovo processo.
-		return new;
-	} else {
-		return NULL;
+/* alloc a new empty pcb (as a child of p_parent) */
+/* p_parent cannot be NULL */
+struct pcb_t *proc_alloc(struct pcb_t *p_parent)
+{
+	if(p_parent == NULL) return NULL; //Error case 1: a process cannot have no parent
+	if(list_empty(&pcb_3)) return NULL; //Error case 2: no free process in list
+	
+	struct pcb_t *alloc_pcb = container_of(&pcb_3->next, struct pcb_t, p_siblings); //Create pointer to first unused item in free list
+
+	list_del(&pcb_3->next); //Remove pcb from free list
+
+	alloc_pcb->p_parent=p_parent; //Assign p_parent to pcb
+	INIT_LIST_HEAD(&alloc_pcb->p_threads); //Initialize pcb threads
+	INIT_LIST_HEAD(&alloc_pcb->p_children); //Initialize pcb children
+	list_add(&alloc_pcb->p_siblings, &p_parent->p_children); //Assign pcb to parent's children
+
+	return alloc_pcb; //Return pointer to created pcb
+}
+
+/* delete a process (properly updating the process tree links) */
+/* this function must fail if the process has threads or children. */
+/* return value: 0 in case of success, -1 otherwise */
+int proc_delete(struct pcb_t *oldproc)
+{
+	if(!(list_empty(&oldproc->p_children))) return -1; //Error 1: cannot delete process if 
+	if(!(list_empty(&oldproc->p_threads))) return -1; //it has active threads or children
+
+	list_del(&oldproc->p_siblings); //Remove oldproc from parent`s children list
+	oldproc->p_parent = NULL; //Remove reference to parent
+	
+	list_add(&oldproc->p_siblings, &pcb_3); //Add oldproc back to free list
+
+	return 0;
+}
+
+/* return the pointer to the first child (NULL if the process has no children) */
+struct pcb_t *proc_firstchild(struct pcb_t *proc)
+{
+	if(list_empty(&proc->p_children) return NULL; //Error 1: no children returns NULL
+	
+	return container_of(&(proc->p_children)->next, struct pcb_t, p_siblings); //Return pointer to process' first child
+}
+
+/* return the pointer to the first thread (NULL if the process has no threads) */
+struct tcb_t *proc_firstthread(struct pcb_t *proc)
+{
+	if(list_empty(&proc->p_threads)) return NULL; //Error 1: no threads returns NULL
+	
+	return container_of(&(proc->p_threads)->next, struct tcb_t, t_next); //Return pointer to process' first thread
+}
+
+/****************************************** THREAD ALLOCATION ****************/
+
+/* initialize the data structure */
+void thread_init(void)
+{
+	static struct tcb_t tcb_array[MAXTHREAD]; //Declare static array of tcb of size MAXTHREAD
+
+	INIT_LIST_HEAD(&tcb_3); //Initialize threads free list
+	
+	for(int i = 0; i<MAXTHREAD; i++) //Iterate for MAXTHREAD times
+	{
+		struct tcb_t *new_tcb = &tcb_array[i]; //Pointer to working element of array
+		list_add(&new_tcb->t_next, &tcb_3); //Use t_next to manage tcb free list
 	}
 }
 
-int proc_delete(struct pcb_t *oldproc){
-	if(!list_empty(&(oldproc->p_children))){
-		return(-1);
-	} else if(!list_empty(&(oldproc->p_threads))){
-		return(-1);
-	} else {
-		// Stacco i collegamenti ai fratelli
-		list_del(&(oldproc->p_siblings));
-		// Stacco il puntatore al padre.
-		oldproc->p_parent = NULL;
-		//Aggiungo il processo alla lista libra.
-		list_add_tail(oldproc, &listfree_head);
-		return 0;
+/* alloc a new tcb (as a thread of process) */
+/* return -1 if process == NULL or mo more available tcb-s.
+	 return 0 (success) otherwise */
+struct tcb_t *thread_alloc(struct pcb_t *process)
+{
+	if(process == NULL) return NULL; //Error 1: process cannot be NULL
+	if(list_empty(&tcb_3)) return NULL; //Free threads list is empty
+
+	struct tcb_t * alloc_tcb = container_of(&tcb_3->next, struct tcb_t, t_next);
+
+	list_del(&tcb_3->next); // remove the new tcb from the free threads list.
+
+	alloc_tcb->t_pcb = process; //link the new tcb to the process.
+
+	INIT_LIST_HEAD(&t_next);
+	INIT_LIST_HEAD(&t_sched);
+	INIT_LIST_HEAD(&t_msgq);
+
+	list_add(&(alloc_tcb->t_next), &(process->p_threads));
+
+	return alloc_tcb; //Return reference to the new thread for success
+}
+
+/* Deallocate a tcb (unregistering it from the list of threads of
+	 its process) */
+/* it fails if the message queue is not empty (returning -1) */
+int thread_free(struct tcb_t *oldthread)
+{
+	if(!(list_empty(&(oldthread->t_msgq))) return -1; //Error 1: message queue not empty
+
+	list_del(&(oldthread->t_next));
+
+	list_add(&(oldthread->t_next), &tcb_3);
+
+	return 0;
+
+}
+
+/*************************** THREAD QUEUE ************************/
+
+/* add a tcb to the scheduling queue */
+void thread_enqueue(struct tcb_t *new, struct list_head *queue)
+{
+	list_add(&(new->t_sched), &queue);
+}
+
+/* return the head element of a scheduling queue.
+	 (this function does not dequeues the element)
+	 return NULL if the list is empty */
+struct tcb_t *thread_qhead(struct list_head *queue)
+{
+	if(list_empty(&queue)) return NULL; //Error 1: queue is empty
+
+	struct tcb_t * thread_c = container_of(&queue, struct tcb_t, t_sched);
+
+	struct list_head * temp_list = thread_c->t_pcb->p_threads.next;
+	return container_of(temp_list, struct tcb_t, t_next);
+}
+
+/* get the first element of a scheduling queue.
+	 return NULL if the list is empty */
+struct tcb_t *thread_dequeue(struct list_head *queue)
+{
+	if(list_empty(&queue)) return NULL; //Error 1: queue is empty
+
+	list_del(&queue);
+
+	return container_of(&queue, struct tcb_t, t_sched);
+	
+}
+
+/*************************** MSG QUEUE ************************/
+
+/* initialize the data structure */
+/* the return value is the address of the root process */
+void msgq_init(void)
+{
+	static struct msg_t msg_array[MAXMSG]; //Declare static array of msg of size MAXMSG
+
+	INIT_LIST_HEAD(&msg_3); //Initialize messages free list
+	
+	for(int i = 0; i<MAXMSG; i++) //Iterate for MAXTHREAD times
+	{
+		struct msg_t *new_msg = &msg_array[i]; //Pointer to working element of array
+		list_add(&(msg_tcb->m_next), &msg_3); //Use m_next to manage msg free list
 	}
+
 }
 
-struct pcb_t *proc_firstchild(struct pcb_t *proc){
-	return proc->p_children.next;
+/* add a message to a message queue. */
+/* msgq_add fails (returning -1) if no more msgq elements are available */
+int msgq_add(struct tcb_t *sender, struct tcb_t *destination, uintptr_t value)
+{
+	if(list_empty(&msg_3)) return -1;
+
+	struct msg_t * new_msg = container_of(&msg_3->next, struct msg_t, m_next);
+
+	list_del(&msg_3->next);
+
+	new_msg->m_sender = sender;
+	m_value = value;
+
+	list_add_tail(&(new_msg->m_next), &(destination->t_msgq));
+
+	return 0;
 }
 
-struct tcb_t *proc_firstthread(struct pcb_t *proc){
-	return proc->p_threads.next;
-}
-
-//---------------threads-----------------------------
-
-void thread_init(void){
+/* retrieve a message from a message queue */
+/* -> if sender == NULL: get a message from any sender
+	 -> if sender != NULL && *sender == NULL: get a message from any sender and store
+	 the address of the sending tcb in *sender
+	 -> if sender != NULL && *sender != NULL: get a message sent by *sender */
+/* return -1 if there are no messages in the queue matching the request.
+	 return 0 and store the message payload in *value otherwise. */
+int msgq_get(struct tcb_t **sender, struct tcb_t *destination, uintptr_t *value)
+{
 	
 }
 
 
 
-//-------------- MAIN ---------------
-int main{
-ptr_pcb_t root = proc_init();
 
 
-}
+
+
+
+
+
+
+
+
+
